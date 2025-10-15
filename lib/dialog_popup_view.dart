f84e75b9d31a9f8cc49928b84ced7722dbabfcd5
Kawsar <alkawsar1030@gmail.com>
2025-10-15 17:42:57 +0600

    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('hello snacbar')))

--- lib/dialog_popup_view.dart
+++ lib/dialog_popup_view.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class DialogPopupView extends StatelessWidget {
@@ -9,82 +7,96 @@ class DialogPopupView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('DialogWorkShop')),
-      body:Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: (){
-              showDialog(context: context, builder: (context)=> AlertDialog(
-               title: Text('Alert again'),
-                actions: [
-                  TextButton(onPressed: (){
-                    Navigator.pop(context);
-                  }, child: Text('Cancel'))
-                ],
-              ));
-            }, child: Text('clickME'),
-            ),
-            ElevatedButton(onPressed: (){
-             showDialog(context: context, builder: (context)=>SimpleDialog(
-               title: Text('selec Item'),
-               children: [
-
-               SimpleDialogOption(
-                child: Text('dhaka'),
-                 onPressed: (){
-                  print("You Have Choosen Dhaka");
-                 },
-               ),SimpleDialogOption(
-                child: Text('khulna'),
-                 onPressed: (){
-                  print("You Have Choosen Khulna");
-                 },
-               ),SimpleDialogOption(
-                child: Text('bhola'),
-                 onPressed: (){
-                  print("You Have Choosen BHola");
-                 },
-               ),SimpleDialogOption(
-                child: Text('bangladesh'),
-                 onPressed: (){
-                  print("You Have Choosen Bangladesh");
-                 },
-               ),
-               ],
-
-               backgroundColor: Colors.lightGreen,
-             ),
-              );
-            }, child: Text('Click on Simple Dialog'),
-            ),
-            OutlinedButton(onPressed: (){
-              showModalBottomSheet(
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
                   context: context,
-                  builder: (context)=>Padding(
+                  builder: (context) => AlertDialog(
+                    title: Text('Alert again'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Cancel'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Text('clickME'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => SimpleDialog(
+                    title: Text('selec Item'),
+                    children: [
+                      SimpleDialogOption(
+                        child: Text('dhaka'),
+                        onPressed: () {
+                          print("You Have Choosen Dhaka");
+                        },
+                      ),
+                      SimpleDialogOption(
+                        child: Text('khulna'),
+                        onPressed: () {
+                          print("You Have Choosen Khulna");
+                        },
+                      ),
+                      SimpleDialogOption(
+                        child: Text('bhola'),
+                        onPressed: () {
+                          print("You Have Choosen BHola");
+                        },
+                      ),
+                      SimpleDialogOption(
+                        child: Text('bangladesh'),
+                        onPressed: () {
+                          print("You Have Choosen Bangladesh");
+                        },
+                      ),
+                    ],
 
+                    backgroundColor: Colors.lightGreen,
+                  ),
+                );
+              },
+              child: Text('Click on Simple Dialog'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) => Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: Container(
                       width: 350,
                       height: 300,
 
-
                       child: Column(
-
                         children: [
                           Text('Write Here Your Script'),
                           Icon(Icons.account_balance_wallet),
-
                         ],
                       ),
-
                     ),
                   ),
-              );
-            }, child: Text('click'),
+                );
+              },
+              child: Text('click'),
             ),
+            ElevatedButton(onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('hello snacbar')));
+            }, child: Text('snackBtn')),
           ],
         ),
-      )
+      ),
     );
   }
 }
